d79de80c4e0a72fe08ef6721660a76ff81fb41fb
@mailsonanselmo <mailson.anselmo@gmail.com>
2022-05-06 00:49:11 -0300

    Add files via upload
    Start Project

---
+++ src/main/java/br/com/seuma/estoquezero/EstoquezeroApplication.java
@@ -0,0 +1,13 @@
+package br.com.seuma.estoquezero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoquezeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoquezeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/seuma/estoquezero/EstoquezeroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.seuma.estoquezero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoquezeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
